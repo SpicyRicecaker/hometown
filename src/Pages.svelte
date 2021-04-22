6fa36186deb8c0152ddbc662b38a5f3adbbba4f5
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-04-21 17:12:22 -0700

    finished adding deletion for pages

--- src/Pages.svelte
+++ src/Pages.svelte
@@ -1,12 +1,19 @@
 <script>
-  import { pages } from './stores';
+  import { pages, editing } from './stores';
   import LinkPage from './LinkPage.svelte';
-  
+
+  const removeElement = (i: number) => {
+    $pages.splice(i, 1);
+    $pages = $pages;
+  };
 </script>
 
-{#each $pages as page}
+{#each $pages as page, i}
   <div class="pages">
     <LinkPage bind:links={page.links} />
+    {#if $editing}
+      <div class="x" on:click={() => removeElement(i)}>×</div>
+    {/if}
   </div>
 {/each}
 
@@ -22,5 +29,20 @@
     height: 100%;
 
     scroll-snap-align: end;
+
+    position: relative;
+  }
+
+  .x {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    right: 0.5rem;
+    color: #ea6962;
+    &:hover {
+      cursor: pointer;
+    }
+
+    font-size: 3rem;
   }
 </style>
